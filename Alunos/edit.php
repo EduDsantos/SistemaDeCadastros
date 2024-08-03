ac41beec0350c493a9d5c55fba73f53ad3ec98d3
Eduardo dos Santos <111947576+EduDsantos@users.noreply.github.com>
2024-08-02 22:07:43 -0300

    Começando o projeto do zero utilizando NodeJs

--- Alunos/edit.php
+++ Alunos/edit.php
@@ -27,7 +27,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrar Aluno</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../TelaRegistroAlunos/style.css">
 </head>
 <body>
 
